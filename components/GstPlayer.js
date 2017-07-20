a4c4c811e473e53ba8bec212f20b5b96c8d2a4d5
Ludovic Bouguerra <ludovic.bouguerra@kalyzee.com>
2017-07-20 22:25:59 +0200

    Remove unused application file + Update Package json info

---
+++ components/GstPlayer.js
@@ -0,0 +1,13 @@
+
+'use strict';
+import { PropTypes } from 'react';
+import { NativeModules, requireNativeComponent, View } from 'react-native';
+
+var iface = {
+  name: 'GstPlayer',
+    ...View.propTypes // include the default view properties
+  },
+};
+var GstPlayer = requireNativeComponent('GstPlayer', iface);
+
+export default GstPlayer;
