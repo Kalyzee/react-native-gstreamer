894c325a71c0f7e85725d62ecc83622997220279
alann.sapone@kalyzee.com <asapone@Alanns-MBP.kast-server.lan>
2018-10-02 08:56:37 +0200

    Delegated event handling

---
+++ ios/RCTGstPlayer/RCTGstPlayerEventDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RCTGstPlayerEventDelegate.h
+//  RCTGstPlayer
+//
+//  Created by Alann Sapone on 01/10/2018.
+//  Copyright © 2018 Kalyzee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTGstPlayerEventDelegate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
